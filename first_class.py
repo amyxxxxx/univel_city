183acf7e1e73e4faa84d49305a040847d6d26b0e
Favor <fayvz.xo@gmail.com>
2021-09-09 15:22:10 +0100

    initial commit

---
+++ first_class.py
@@ -0,0 +1,68 @@
+# welcome = 5
+
+# INT = 18
+# STR = "cool"
+# FLOAT = 5.5
+# BOOL = False
+
+# type_of_data = type(BOOL)
+# print(type_of_data)
+
+# print(type(INT))
+
+# LIST = []
+
+a = 3
+b = 6
+c = 3
+numerator = -b+(b**2 - 4*a*c)**0.5
+denominator = 2*a
+
+x = numerator/denominator
+print(x)
+
+numerator = 5**0.5 +2
+denominator = 15**(4**0.5)
+
+x = numerator/denominator
+print(x)
+
+print(a is b)
+print(a is not c)
+
+P = 10000
+n = 12
+r = 0.08
+t = 5
+A = P*((1+r/n)**(n*t))
+print(A)
+
+a = 4
+b = 9
+c = 8
+s = a+b+c
+print(s)
+
+Area = (s*(s-a)*(s-b)*(s-c))**0.5
+print(Area)
+
+# (age) man = m:57
+# (age)daughter = d
+# m ==2*d
+# print(d)
+
+# AI = 100000
+# A = 0.1*AI
+# Total = A*12
+# print(total)
+
+
+man_age = 47 + 10
+daughter = man_age//2
+print(daughter)
+
+income = 100000
+saving_per_month = income*0.1
+total = saving_per_month*12
+print(total)
+
