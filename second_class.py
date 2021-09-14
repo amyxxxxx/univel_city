bff471d46fefb4e324a48555f013c96c1b90a26b
Favor <fayvz.xo@gmail.com>
2021-09-14 16:14:14 +0100

    first commit

---
+++ second_class.py
@@ -0,0 +1,88 @@
+# print("beautiful" + "city")
+
+# name = "Favor"
+# print(name[3])
+# print(name[-5])
+
+# firstname = "Favor"
+# lastname = "Bekeh"
+# fullname = firstname + " " + lastname
+# print(fullname)
+
+# print(fullname[6:11])
+# print(fullname[-6:])
+# print(fullname[:2])
+# # print(fullname[0:11:3])
+
+# name = "Amy"
+# price = "500"
+# print(f"welcome {name}")
+# print(f"welcome {name}. You are to pay NGN{price}")
+# print("welcome {}. You have just signed in". format(name))
+
+# name = input("Tell us who you are: ")
+# print("Welcome {}. You have successfully signed in". format(name))
+# print(f"Welcome {name}. You have successfully signed in")
+
+# name = input("Please input your name: ")
+# age = int(input("How old are you: "))
+# date_of_birth = 2021-age
+
+# print("Hello {}. You were born in the year {}.". format(name,date_of_birth))
+
+# print("Hey, {}.\nYou have signed in successfully.\n\tSign:\n\tManagement".format(name))
+# print('her father\'s daughter')
+
+# name = input("Tell us who you are: ")
+# print(name.title())
+
+# name = "Radjou_pujalte".upper()
+# print(name)
+# print(name.startswith("R"))
+# print(name.endswith("e"))
+
+# print(name.find('u_p'))
+# print(name.index('u_p'))
+# print(name.find('lol'))
+# print(name.index('lol'))
+
+# s = "    "
+# print(s.isspace())
+
+# x = "a12s"
+# print(x.isalpha())
+
+# fullname = "Favor Bekeh"
+# name_list = fullname.split()
+# print(name_list)
+# print(name_list[1])
+
+# word_list = ['Favor', 'Bekeh']
+# joined = "-".join(word_list)
+# print(joined)
+
+# stringed = 'I am a girl'
+# namelist = stringed.split()
+# print(namelist)
+# joined = "-".join(namelist)
+# print(joined)
+
+# word = "I am a girl"
+# word = word.replace(" ", "-")
+# print(word)
+
+# song_lyrics = """Happy birthday to you, happy birthday to you, happy birthday to you sister, HAPPY birthday to you.""".lower()
+# word = input("Choose a word from song_lyrics:\n").lower()
+# print(f"{song_lyrics.count(word)}found")
+
+# final = song_lyrics.replace(word, word.upper())
+# print(final)
+
+# x = song_lyrics.replace(word, word.upper())
+# s = print(word)
+# c = song_lyrics.count('s')
+# print(c)
+# x = song_lyrics.replace(s,'s'.upper())
+# print(x)
+# y = song_lyrics.count(s)
+
