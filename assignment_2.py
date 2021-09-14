a9bc36327d5d74656cf3de102ad5c3bf1d8810be
Favor <fayvz.xo@gmail.com>
2021-09-14 21:08:33 +0100

    first commit

---
+++ assignment_2.py
@@ -0,0 +1,27 @@
+s1 = "Ault"
+s2 = "Kelly"
+first = s1[:2]
+second = s2[:]
+third = s1[2:]
+new_string = first+second+third
+print(new_string)
+
+s1 = "America"
+s2 = "Japan"
+first = s1[:1]
+second = s2[:1]
+third = s1[3]
+fourth = s2[2:5]
+new_string = first+second+third+fourth
+print(new_string)
+
+s1 = "Abc"
+s2 = "Xyz"
+first = s1[0]
+second = s2[-1]
+third = s1[1]
+fourth = s2[-2]
+fifth = s1[2]
+sixth = s2[-3]
+new_string = first+second+third+fourth+fifth+sixth
+print(new_string)
