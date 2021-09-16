48827dfb1f867599b1906752182e676e9701b45a
Favor <fayvz.xo@gmail.com>
2021-09-16 16:04:27 +0100

    list

--- assignment_2.py
+++ assignment_2.py
@@ -1,27 +1,55 @@
-s1 = "Ault"
-s2 = "Kelly"
-first = s1[:2]
-second = s2[:]
-third = s1[2:]
-new_string = first+second+third
-print(new_string)
+# s1 = "Ault"
+# s2 = "Kelly"
+# first = s1[:2]
+# second = s2[:]
+# third = s1[2:]
+# new_string = first+second+third
+# print(new_string)
 
-s1 = "America"
-s2 = "Japan"
-first = s1[:1]
-second = s2[:1]
-third = s1[3]
-fourth = s2[2:5]
-new_string = first+second+third+fourth
-print(new_string)
+# s1 = "America"
+# s2 = "Japan"
+# first = s1[:1]
+# second = s2[:1]
+# third = s1[3]
+# fourth = s2[2:5]
+# new_string = first+second+third+fourth
+# print(new_string)
+
+# s1 = "Abc"
+# s2 = "Xyz"
+# first = s1[0]
+# second = s2[-1]
+# third = s1[1]
+# fourth = s2[-2]
+# fifth = s1[2]
+# sixth = s2[-3]
+# new_string = first+second+third+fourth+fifth+sixth
+# print(new_string)
+
+# s1 = "Ault"
+# s2 = "Kelly"
+# s = int(len(s1)/2)
+# solution = s1[0:s] + s2 + s1[s:]
+# print(solution)
+
+# s1 = "America"
+# s2 = "Japan"
+# a = s1[0]
+# b = s2[0]
+# c = s1[len(s1)//2]
+# d = s2[len(s2)//2]
+# e = s1[-1]
+# f = s2[-1]
+# solution = a+b+c+d+e+f
+# print(solution)
 
 s1 = "Abc"
 s2 = "Xyz"
-first = s1[0]
-second = s2[-1]
-third = s1[1]
-fourth = s2[-2]
-fifth = s1[2]
-sixth = s2[-3]
-new_string = first+second+third+fourth+fifth+sixth
-print(new_string)
+a = s1[0]
+b = s2[-1]
+c = s1[1]
+d = s2[-2]
+e = s1[2]
+f = s2[-3]
+solution = a+b+c+d+e+f
+print(solution)
