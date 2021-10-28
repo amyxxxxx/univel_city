2645247a9b3b4e01a4e48c056fb11e2463791194
Favor <fayvz.xo@gmail.com>
2021-10-28 15:57:47 +0100

    reportly app

--- reportly/reportly/attendance/admin.py
+++ reportly/reportly/attendance/admin.py
@@ -3,12 +3,20 @@ from .models import Book, Student
 
 # Register your models here.
 # admin.site.register(Book)
-admin.site.register(Student)
+# admin.site.register(Student)
 
 @admin.register(Book)
 class BookAdmin(admin.ModelAdmin):
-    list_display = ['title', 'no_of_pages', 'date']
+    list_display = ['title', 'id', 'no_of_pages', 'date']
     list_editable = [ ]
     list_filter = ['date']
     list_per_page = 2
     search_fields = ['title', 'body', 'author']
+
+@admin.register(Student)
+class StudentAdmin(admin.ModelAdmin):
+    list_display = ['name', 'id']
+    list_editable = []
+    list_filter = []
+    search_fields = ['name', 'age', 'date_of_birth']
+
--- reportly/reportly/attendance/models.py
+++ reportly/reportly/attendance/models.py
@@ -1,17 +1,6 @@
 from django.db import models
 
 # Create your models here.
-class Book(models.Model):
-    title = models.CharField(max_length=300)
-    no_of_pages = models.IntegerField(default=10)
-    author = models.CharField(max_length=300)
-    body = models.TextField()
-    isbn = models.CharField(max_length=20, null=True, blank=True)
-    date = models.DateTimeField()
-
-    def __str__(self):
-        return self.title
-
 class Student(models.Model):
     name = models.CharField(max_length=25)
     age = models.PositiveIntegerField()
@@ -21,3 +10,27 @@ class Student(models.Model):
 
     def __str__(self):
         return self.name
+
+# class Book(models.Model):
+#     title = models.CharField(max_length=300)
+#     no_of_pages = models.IntegerField(default=10)
+#     author = models.CharField(max_length=300)
+#     student = models.OneToOneField(Student, on_delete=models.CASCADE)
+#     body = models.TextField()
+#     isbn = models.CharField(max_length=20, null=True, blank=True)
+#     date = models.DateTimeField(auto_now_add=True)
+
+#     def __str__(self):
+#         return self.title
+
+class Book(models.Model):
+    title = models.CharField(max_length=300)
+    no_of_pages = models.IntegerField(default=10)
+    author = models.CharField(max_length=300)
+    student = models.ForeignKey(Student, on_delete=models.CASCADE, related_name="books")
+    body = models.TextField()
+    isbn = models.CharField(max_length=20, null=True, blank=True)
+    date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
